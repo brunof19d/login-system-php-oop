60aa3804e4da20b757d75b20c4f4b20e884185ad
brunof19d <brunof19d@gmail.com>
2020-09-06 22:00:42 +0100

    make insert SQL function

--- src/model/UserRepository.php
+++ src/model/UserRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author           Bruno Dadario <brunof19d@gmail.com>
  * @copyright        (c) 2020, Bruno Dadario. All Rights Reserved.
@@ -8,8 +9,23 @@
 
 class UserRepository
 {
-    public function __construct(PDO $pdo) 
+
+    private $pdo;
+
+    public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
+    }
+
+    public function registerUser(User $user)
+    {
+        $sqlInsert = "INSERT INTO user_login (email_user, password_user) VALUES (:email_user, :password_user)";
+
+        $statement = $this->pdo->prepare($sqlInsert);;
+
+        $statement->execute([
+            'email_user'    => strip_tags($user->getEmail()),
+            'password_user' => strip_tags($user->getPassword())
+        ]);
     }
 }
