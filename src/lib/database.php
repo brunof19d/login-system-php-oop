338969bb63f84d0409475dd0a6a6e16f9bbedc02
brunof19d <brunof19d@gmail.com>
2020-09-07 03:08:49 +0100

    error mode PDO

--- src/lib/database.php
+++ src/lib/database.php
@@ -12,6 +12,7 @@ $password = '';
 
 try {
     $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
