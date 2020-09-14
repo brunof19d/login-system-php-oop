499590b6367525fac588e73247ee643e1fed1da3
brunof19d <brunof19d@gmail.com>
2020-09-14 20:42:27 +0100

    update system

--- config.php
+++ config.php
@@ -1,3 +1,16 @@
 <?php
+/* Require autoload Composer */
+require "vendor/autoload.php";
 
+/* Composer */
+use Login\App\Controller\AdminController;
+use Login\App\Domain\Model\User;
+use Login\App\Infrastructure\Repository\PdoUserRepository;
+
+/* Config site */
 define('SITE_URL', 'http://localhost/login-system-php-oo/'); // Put your URL project here
+
+/* Call class using for controller */
+$user = new User();
+$result = new PdoUserRepository();
+$app = new AdminController();
--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -1,35 +1,21 @@
 <?php
-/* Require autoload Composer */
-require "vendor/autoload.php";
+
 /* Config */
 require_once "config.php";
 
-use Login\App\Domain\Model\User;
-use Login\App\Infrastructure\Repository\PdoUserRepository;
-
-$user = new User();
-$result = new PdoUserRepository();
-
 /* Controller Register User */
+
 try {
     if (isset($_POST['register_user'])) {
-
-        $email = filter_var($_POST['input_email'], FILTER_VALIDATE_EMAIL);
-        $password = filter_var($_POST['input_password'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-        if ($email === false) {
-            throw new Exception('Email invalid');
-        }
-
-        $user->setEmail($email);
-        $user->setPassword($password);
-        $result->save($user);
-
+      $app->persist();
     }
 } catch (Exception $error) {
     echo $error->getMessage();
 }
 
+/* List of Users */
 $userList = $result->allUsers();
+
+
 
 
