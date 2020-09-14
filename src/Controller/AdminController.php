3c3112caea229c85c066dd2b9a1955b20d235545
brunof19d <brunof19d@gmail.com>
2020-09-14 20:42:10 +0100

    make class controller admin,which will validate data

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,27 @@
 namespace Login\App\Controller;
 
 
+use Exception;
+use Login\App\Domain\Model\User;
+use Login\App\Infrastructure\Repository\PdoUserRepository;
+
 class AdminController
 {
+    public function persist()
+    {
+        $user = new User();
+        $result = new PdoUserRepository();
 
+        $email = filter_var($_POST['input_email'], FILTER_VALIDATE_EMAIL);
+        $password = filter_var($_POST['input_password'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+        if ($email === false) {
+            throw new Exception('Email invalid');
+        }
+
+        $user->setEmail($email);
+        $user->setPassword($password);
+        $result->save($user);
+
+    }
 }
